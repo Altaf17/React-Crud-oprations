5dc18d591f1eb3bba6ecd25c8ba160f0d127b8b3
Altaf Hussain <altafsky17@gmail.com>
2023-03-11 17:55:17 +0530

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import EmployeeList from './Components/EmployeeList';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import EmployeeCreate from './Components/EmployeeCreate';
+import EmployeeDetail from './Components/EmployeeDetail';
+import EmployeeEdit from './Components/EmployeeEdit';
+import EmpNavbar  from './Components/EmpNavbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{
+      backgroundImage:" url(https://www.affordablebackgroundchecks.com/blog/wp-content/uploads/2019/03/Employee-Personal-Details-Check-1200x675.jpg)",
+      backgroundColor: "#66b3ff",
+      backgroundRepeat: "no-repeat",
+      backgroundSize:"cover"
+    }}>
+      <EmpNavbar/>
+      <h1 className='text-center' style={{color:"#ff9933"}}>Skyline-Employee services Pvt-Ltd</h1>
+      <h3  className='text-center' style={{color:"#ff99ff"}}>CRUD Operations in React</h3>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<EmployeeList />} />
+          <Route path='/employee/create' element={<EmployeeCreate />} />
+          <Route path='/employee/detail/:empid' element={<EmployeeDetail />} />
+          <Route path='/employee/edit/:empid' element={<EmployeeEdit />} />
+        </Routes>
+      </BrowserRouter>
+
+
     </div>
   );
 }
